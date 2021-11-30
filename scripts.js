c26148d297b3e54614ae75288607a2bc454526e3
Kristian Workinger <kristianworkinger@gmail.com>
2021-11-29 16:45:36 -0800

    changes

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ let firstCard, secondCard
 function flipCard(){
     if (lockBoard) return
     if (this === firstCard) return
-    
+
     this.classList.add('flip')
 
     if(!hasFlippedCard) {
@@ -17,8 +17,7 @@ function flipCard(){
         return
     }   
     
-    secondCard = this;
-        
+    secondCard = this; 
     checkForMatch()
     }
 
@@ -31,19 +30,19 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard)
     secondCard.removeEventListener('click', flipCard)
+
     resetBoard()
 }
 
 function unflipCards() {
     lockBoard = true
+    
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
 
-        lockBoard = false;
+        resetBoard()
     }, 1500)
-
-    resetBoard()
 }
 
 function resetBoard() {
@@ -56,6 +55,6 @@ function resetBoard() {
         let randomPos = Math.floor(Math.random()*12)
         card.style.order = randomPos
     })
-})
+})()
 
 cards.forEach(card => card.addEventListener('click', flipCard))
